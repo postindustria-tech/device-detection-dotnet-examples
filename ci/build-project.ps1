a5a7c9e1c7d1f84a016ff787cb31a572f552733a
Artur51D <106963910+Artur51D@users.noreply.github.com>
2023-05-25 11:54:37 +0100

    Update build-project.ps1

--- ci/build-project.ps1
+++ ci/build-project.ps1
@@ -10,8 +10,36 @@ param(
 
 if ($BuildMethod -eq "dotnet"){
     Write-Output "$Configuration"
+    $Projects = @( 
+    ".\Examples\ExampleBase\FiftyOne.DeviceDetection.Examples.csproj",
+    ".\Examples\Legacy Web\Cloud-UACH\Cloud - Client-Hints.csproj",
+    ".\Examples\Legacy Web\Cloud-UACH-manual\Cloud - Client-Hints Not Integrated.csproj",
+    ".\Examples\Legacy Web\UACH\Client-Hints.csproj",
+    ".\Examples\Legacy Web\UACH-manual\Client-Hints Not Integrated.csproj",
+    ".\Examples\Cloud\Configurator-Console\Configurator-Console.csproj",
+    ".\Examples\Cloud\GetAllProperties-Console\GetAllProperties.csproj",
+    ".\Examples\Cloud\GettingStarted-Console\GettingStarted-Console.csproj",
+    ".\Examples\Cloud\GettingStarted-Web\GettingStarted-Web.csproj",
+    ".\Examples\Cloud\Metadata-Console\Metadata-Console.csproj",
+    ".\Examples\Cloud\NativeModel-Console\NativeModelLookup-Console.csproj",
+    ".\Examples\Cloud\TAC-Console\TacLookup-Console.csproj",
+    ".\Examples\OnPremise\AppleServerSideConfig-Console\AppleServerSideConfig-Console.csproj",
+    ".\Examples\OnPremise\AppleServerSide-Console\AppleServerSide-Console.csproj",
+    ".\Examples\OnPremise\GettingStarted-Console\GettingStarted-Console.csproj",
+    ".\Examples\OnPremise\GettingStarted-Web\GettingStarted-Web.csproj",
+    ".\Examples\OnPremise\MatchMetrics-Console\MatchMetrics-Console.csproj",
+    ".\Examples\OnPremise\Metadata-Console\Metadata-Console.csproj",
+    ".\Examples\OnPremise\OfflineProcessing-Console\OfflineProcessing-Console.csproj",
+    ".\Examples\OnPremise\Performance-Console\Performance-Console.csproj",
+    ".\Examples\OnPremise\UpdateDataFile-Console\UpdateDataFile-Console.csproj",
+    ".\Tests\FiftyOne.DeviceDetection.Example.Tests\FiftyOne.DeviceDetection.Example.Tests.csproj",
+    ".\Tests\FiftyOne.DeviceDetection.Example.Tests.Web\FiftyOne.DeviceDetection.Example.Tests.Web.csproj"
+    )
 
-    ./dotnet/build-project-core.ps1 -RepoName $RepoName -ProjectDir "FiftyOne.DeviceDetection.Examples.sln" -Name $Name -Configuration $Configuration -Arch $Arch
+    foreach($Project in $Projects){
+        ./dotnet/build-project-core.ps1 -RepoName $RepoName -ProjectDir $Project -Name $Name -Configuration $Configuration -Arch $Arch
+    }
+
 
 }
 else{

40cd6900923f7334e717c9198e2df42da6a69c85
Artur51D <106963910+Artur51D@users.noreply.github.com>
2023-06-09 15:51:14 +0100

    Update GettingStartedOnPremiseEdgeTest.cs

--- Tests/FiftyOne.DeviceDetection.Example.Tests.Web/GettingStartedOnPremiseEdgeTest.cs
+++ Tests/FiftyOne.DeviceDetection.Example.Tests.Web/GettingStartedOnPremiseEdgeTest.cs
@@ -41,162 +41,150 @@ namespace FiftyOne.DeviceDetection.Example.Tests.Web
     [TestClass]
     public class GettingStartedOnPremiseEdgeTest : SeleniumTestsBase
     {
-        private const string STATIC_HTML_ENDPOINT = "/static.html";
-        private const string TEST_PAGE_ENDPOINT = "/testpage.html";
-
-        [TestInitialize]
-        public void Init()
+        [TestClass]
+        public class GettingStartedOnPremiseChromeTest : SeleniumTestsBase
         {
-            base.InitializeChromeDriver();
-        }
+            private const string STATIC_HTML_ENDPOINT = "/static.html";
+            private const string TEST_PAGE_ENDPOINT = "/testpage.html";
+            private CancellationTokenSource _stopToken;
 
-        /// <summary>
-        /// Ports on which the example is listening are defined in <see cref="Constants"/>
-        /// </summary>
-        public static IEnumerable<object[]> UrlsData
-        {
-            get
+
+            [TestInitialize]
+            public void Init()
             {
-                return new[]
+                base.InitializeChromeDriver();
+                // Arrange
+                _stopToken = new CancellationTokenSource();
+                var serverTask = Task.Run(() =>
+                    Examples.OnPremise.GettingStartedWeb.Program.Main(
+                        new string[] { }),
+                        _stopToken.Token);
+            }
+
+            /// <summary>
+            /// Ports on which the example is listening are defined in <see cref="Constants"/>
+            /// </summary>
+            public static IEnumerable<object[]> UrlsData
+            {
+                get
                 {
+                    return new[]
+                    {
                     new object[] { $"https://localhost:{Constants.LOCALHOST_HTTPS_PORTS[0]}" },
                     new object[] { $"https://localhost:{Constants.LOCALHOST_HTTPS_PORTS[1]}" }
                 };
+                }
             }
-        }
-        /// <summary>
-        /// Verifies that the 51d cookie is populated.
-        /// </summary>
-        /// <param name="url"></param>
-        [DataTestMethod]
-        [DynamicData(nameof(UrlsData))]
-        public void VerifyExample_GetHighEntropyValues_Populates_51D_Cookie(string url)
-        {
-            // Arrange
-            var stopToken = new CancellationTokenSource();
-            var serverTask = Task.Run(() =>
-                Examples.OnPremise.GettingStartedWeb.Program.Main(
-                    new string[] { }),
-                    stopToken.Token);
 
-            using (Driver)
+            /// <summary>
+            /// Verifies that the 51d cookie is populated.
+            /// </summary>
+            /// <param name="url"></param>
+            [DataTestMethod]
+            [DynamicData(nameof(UrlsData))]
+            public void VerifyExample_GetHighEntropyValues_Populates_51D_Cookie(string url)
             {
-                // Enable DevTools
-                var devTools = Driver as IDevTools;
-                var session = devTools.GetDevToolsSession();
-
-                var domains = session.GetVersionSpecificDomains<OpenQA.Selenium.DevTools.V113.DevToolsSessionDomains>();
-                domains.Network.Enable(new OpenQA.Selenium.DevTools.V113.Network.EnableCommandSettings());
-
-
-                // Act
-                Driver.Navigate().GoToUrl(url + STATIC_HTML_ENDPOINT);
-
-                // Wait for the page to load
-                Thread.Sleep(TimeSpan.FromSeconds(3));
-
-                var cookies = domains.Network.GetAllCookies().Result;
-                var fod_cookie = cookies.Cookies.Where(c => c.Name == "51D_GetHighEntropyValues").Single();
-                var bytes = new Span<byte>(new byte[1024]);
-
-                // Assert
-                Assert.IsNotNull(fod_cookie);
-                Assert.IsTrue(Convert.TryFromBase64String(fod_cookie.Value, bytes, out int _));
-
-                // Quit the driver
-                Driver.Quit();
-            }
-            stopToken.Cancel(false);
-        }
-
-        [TestMethod]
-        public async Task VerifyExample_GetHighEntropyValues_Contains_CORS_Response_Header()
-        {
-            // Arrange
-            var stopToken = new CancellationTokenSource();
-            var serverTask = Task.Run(() =>
-                Examples.OnPremise.GettingStartedWeb.Program.Main(
-                    new string[] { }),
-                    stopToken.Token);
-
-            using (Driver)
-            {
-                // Enable DevTools
-                var devTools = Driver as IDevTools;
-                var session = devTools.GetDevToolsSession();
-
-                var domains = session.GetVersionSpecificDomains<OpenQA.Selenium.DevTools.V113.DevToolsSessionDomains>();
-                await domains.Network.Enable(new OpenQA.Selenium.DevTools.V113.Network.EnableCommandSettings());
-
-                Dictionary<string, string> headerValuePairs = new();
-
-                // Get Response Headers
-                domains.Network.ResponseReceived += (sender, e) =>
+                using (Driver)
                 {
-                    var headers = e.Response.Headers;
-                    var responseUrl = e.Response.Url;
-                    if (responseUrl.Equals($"{UrlsData.ElementAt(0).Single()}/51dpipeline/json"))
-                    {
-                        foreach (var header in headers)
-                        {
-                            headerValuePairs.Add(header.Key.ToLower(), header.Value);
-                        }
-                    }
-                };
+                    // Enable DevTools
+                    var devTools = Driver as IDevTools;
+                    var session = devTools.GetDevToolsSession();
 
-                // Act
-                // Do a cross origin request
-                var url = UrlsData.ElementAt(1).Single().ToString();
-                Driver.Navigate().GoToUrl(url + STATIC_HTML_ENDPOINT);
+                    var domains = session.GetVersionSpecificDomains<OpenQA.Selenium.DevTools.V113.DevToolsSessionDomains>();
+                    domains.Network.Enable(new OpenQA.Selenium.DevTools.V113.Network.EnableCommandSettings());
 
-                // Wait for the page to load
-                Thread.Sleep(TimeSpan.FromSeconds(3));
 
-                // Assert
-                // Verify that the response contains the header
-                Assert.IsTrue(headerValuePairs.ContainsKey("access-control-allow-origin"));
+                    // Act
+                    Driver.Navigate().GoToUrl(url + STATIC_HTML_ENDPOINT);
 
-                // Quit the driver
-                Driver.Quit();
+                    // Wait for the page to load
+                    Thread.Sleep(TimeSpan.FromSeconds(3));
+
+                    var cookies = domains.Network.GetAllCookies().Result;
+                    var fod_cookie = cookies.Cookies.Where(c => c.Name == "51D_GetHighEntropyValues").Single();
+                    var bytes = new Span<byte>(new byte[1024]);
+
+                    // Assert
+                    Assert.IsNotNull(fod_cookie);
+                    Assert.IsTrue(Convert.TryFromBase64String(fod_cookie.Value, bytes, out int _));
+
+                    // Quit the driver
+                    Driver.Quit();
+                }
             }
-            stopToken.Cancel(false);
-        }
 
-        [DataTestMethod]
-        [DynamicData(nameof(UrlsData))]
-        public void VerifyExample_GetHighEntropyValues_Fod_Completes(string url)
-        {
-            // Arrange
-            var stopToken = new CancellationTokenSource();
-            var serverTask = Task.Run(() =>
-                Examples.OnPremise.GettingStartedWeb.Program.Main(
-                    new string[] { }),
-                    stopToken.Token);
-            using (Driver)
+            [TestMethod]
+            public void VerifyExample_GetHighEntropyValues_Contains_CORS_Response_Header()
             {
-                // Enable DevTools
-                var devTools = Driver as IDevTools;
-                var session = devTools.GetDevToolsSession();
-                IJavaScriptExecutor js = (IJavaScriptExecutor)Driver;
+                using (Driver)
+                {
+                    // Enable DevTools
+                    var devTools = Driver as IDevTools;
+                    var session = devTools.GetDevToolsSession();
 
-                var domains = session.GetVersionSpecificDomains<OpenQA.Selenium.DevTools.V113.DevToolsSessionDomains>();
-                domains.Network.Enable(new OpenQA.Selenium.DevTools.V113.Network.EnableCommandSettings());
+                    var domains = session.GetVersionSpecificDomains<OpenQA.Selenium.DevTools.V113.DevToolsSessionDomains>();
+                    domains.Network.Enable(new OpenQA.Selenium.DevTools.V113.Network.EnableCommandSettings());
 
-                Driver.Navigate().GoToUrl(url + TEST_PAGE_ENDPOINT);
+                    Dictionary<string, string> headerValuePairs = new();
 
-                // This throws an exception if the timeout period elapses,
-                // which will cause the test to fail.
-                var complete = new WebDriverWait(Driver, TimeSpan.FromSeconds(20)).Until(
-                     driver =>
-                         js.ExecuteScript("return test").Equals("complete") &&
-                         js.ExecuteScript("return deviceid").Equals("loading") == false);
+                    // Get Response Headers
+                    domains.Network.ResponseReceived += (sender, e) =>
+                    {
+                        var headers = e.Response.Headers;
+                        var responseUrl = e.Response.Url;
+                        if (responseUrl.Equals($"{UrlsData.ElementAt(0).Single()}/51dpipeline/json"))
+                        {
+                            foreach (var header in headers)
+                            {
+                                headerValuePairs.Add(header.Key.ToLower(), header.Value);
+                            }
+                        }
+                    };
 
+                    // Act
+                    // Do a cross origin request
+                    var url = UrlsData.ElementAt(1).Single().ToString();
+                    Driver.Navigate().GoToUrl(url + STATIC_HTML_ENDPOINT);
 
-                // Quit the _driver
-                Driver.Quit();
+                    // Wait for the page to load
+                    Thread.Sleep(TimeSpan.FromSeconds(3));
+
+                    // Assert
+                    // Verify that the response contains the header
+                    Assert.IsTrue(headerValuePairs.ContainsKey("access-control-allow-origin"));
+
+                    // Quit the driver
+                    Driver.Quit();
+                }
             }
-            stopToken.Cancel(false);
+
+            [DataTestMethod]
+            [DynamicData(nameof(UrlsData))]
+            public void VerifyExample_GetHighEntropyValues_Fod_Completes(string url)
+            {
+                using (Driver)
+                {
+                    // Enable DevTools
+                    var devTools = Driver as IDevTools;
+                    var session = devTools.GetDevToolsSession();
+                    IJavaScriptExecutor js = (IJavaScriptExecutor)Driver;
+
+                    var domains = session.GetVersionSpecificDomains<OpenQA.Selenium.DevTools.V113.DevToolsSessionDomains>();
+                    domains.Network.Enable(new OpenQA.Selenium.DevTools.V113.Network.EnableCommandSettings());
+
+                    Driver.Navigate().GoToUrl(url + TEST_PAGE_ENDPOINT);
+
+                    // This throws an exception if the timeout period elapses,
+                    // which will cause the test to fail.
+                    var complete = new WebDriverWait(Driver, TimeSpan.FromSeconds(20)).Until(
+                         driver =>
+                             js.ExecuteScript("return test").Equals("complete") &&
+                             js.ExecuteScript("return deviceid").Equals("loading") == false);
+
+
+                    // Quit the _driver
+                    Driver.Quit();
+                }
+            }
         }
     }
-
-}

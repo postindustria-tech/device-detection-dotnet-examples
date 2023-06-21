e969e0e00645cd994f40de9cddb58a2bb7dfa82a
James Rosewell <james@51degrees.com>
2023-06-21 12:55:21 +0100

    TEST: Fixed some issues with testing the Razor Pages project for client only device detection testing.

--- Examples/Cloud/GettingStarted-Web-ClientOnly/Program.cs
+++ Examples/Cloud/GettingStarted-Web-ClientOnly/Program.cs
@@ -49,18 +49,14 @@ namespace FiftyOne.DeviceDetection.Examples.Cloud.GettingStartedWeb.ClientOnly
             builder.WebHost.UseUrls(Constants.AllUrls);
 
             // Add services to the container.
-            builder.Services.AddRazorPages();
+            builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
+            if (app.Environment.IsDevelopment() == false)
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change
-                // this for production scenarios, see
-                // https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
 
             app.UseStaticFiles();

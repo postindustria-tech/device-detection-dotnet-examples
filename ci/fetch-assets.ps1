a84a56ce78dcf3714450bbee21db81b3a20201ec
Maksym Kucherov <maximus.the0@gmail.com>
2023-10-24 12:08:58 +0300

    Revert "Try using non-LFS URLs." commits.

--- ci/fetch-assets.ps1
+++ ci/fetch-assets.ps1
@@ -7,52 +7,22 @@ param (
 )
 
 # Fetch the TAC data file for testing with
-# ./steps/fetch-hash-assets.ps1 -RepoName $RepoName -LicenseKey $DeviceDetection -Url $DeviceDetectionUrl
+./steps/fetch-hash-assets.ps1 -RepoName $RepoName -LicenseKey $DeviceDetection -Url $DeviceDetectionUrl
 
 # Move the data file to the correct location
-# $DataFileName = "TAC-HashV41.hash"
-# $DataFileSource = [IO.Path]::Combine($pwd, $RepoName, $DataFileName)
+$DataFileName = "TAC-HashV41.hash"
+$DataFileSource = [IO.Path]::Combine($pwd, $RepoName, $DataFileName)
 $DataFileDir = [IO.Path]::Combine($pwd, $RepoName,"device-detection-data")
-# $DataFileDestination = [IO.Path]::Combine($DataFileDir, $DataFileName)
-# Move-Item $DataFileSource $DataFileDestination
+$DataFileDestination = [IO.Path]::Combine($DataFileDir, $DataFileName)
+Move-Item $DataFileSource $DataFileDestination
 
-$ErrorActionPreference = 'Stop'
-
-$assets = New-Item -ItemType Directory -Path assets -Force
-$deviceDetectionData = $DataFileDir
-
-$downloads = @{
-    "TAC-HashV41.hash" = {
-        ./steps/fetch-hash-assets.ps1 -RepoName $RepoName -LicenseKey $DeviceDetection -Url $DeviceDetectionUrl
-        Move-Item -Path $RepoName/$file -Destination $assets
-    }
-    "51Degrees-LiteV4.1.hash" = {Invoke-WebRequest -Uri "https://storage.googleapis.com/51degrees-assets/$DeviceDetection/51Degrees-LiteV4.1.hash" -OutFile $assets/$file}
-    "20000 Evidence Records.yml" = {Invoke-WebRequest -Uri "https://storage.googleapis.com/51degrees-assets/$DeviceDetection/20000%20Evidence%20Records.yml" -OutFile $assets/$file}
-    "20000 User Agents.csv" = {Invoke-WebRequest -Uri "https://storage.googleapis.com/51degrees-assets/$DeviceDetection/20000%20User%20Agents.csv" -OutFile $assets/$file}
-    "51Degrees.csv" = {
-        Invoke-WebRequest -Uri "https://storage.googleapis.com/51degrees-assets/$DeviceDetection/51Degrees-Tac.zip" -OutFile 51Degrees-Tac.zip
-        Expand-Archive -Path 51Degrees-Tac.zip
-        Get-Content -TotalCount 1 51Degrees-Tac/51Degrees-Tac-All.csv | Out-File $assets/$file # We only need a header
-        Remove-Item -Path 51Degrees-Tac.zip, 51Degrees-Tac/51Degrees-Tac-All.csv
-    }
+# Get the evidence files for testing. These are in the device-detection-data submodule,
+# But are not pulled by default.
+Push-Location $DataFileDir
+try {
+    Write-Output "Pulling evidence files"
+    git lfs pull
 }
-
-foreach ($file in $downloads.Keys) {
-    if (!(Test-Path $assets/$file)) {
-        Write-Output "Downloading $file"
-        Invoke-Command -ScriptBlock $downloads[$file]
-    } else {
-        Write-Output "'$file' exists, skipping download"
-    }
+finally {
+    Pop-Location
 }
-
-# Tests mutate this file, so we copy it
-# Write-Output "Copying 'TAC-HashV41.hash' to '$deviceDetectionData/Enterprise-HashV41.hash'"
-# Copy-Item -Path $assets/TAC-HashV41.hash -Destination $deviceDetectionData/Enterprise-HashV41.hash
-
-# We can just symlink these
-New-Item -ItemType SymbolicLink -Force -Target "$assets/TAC-HashV41.hash" -Path "$deviceDetectionData/TAC-HashV41.hash"
-New-Item -ItemType SymbolicLink -Force -Target "$assets/51Degrees-LiteV4.1.hash" -Path "$deviceDetectionData/51Degrees-LiteV4.1.hash"
-New-Item -ItemType SymbolicLink -Force -Target "$assets/20000 Evidence Records.yml" -Path "$deviceDetectionData/20000 Evidence Records.yml"
-New-Item -ItemType SymbolicLink -Force -Target "$assets/20000 User Agents.csv" -Path "$deviceDetectionData/20000 User Agents.csv"
-# New-Item -ItemType SymbolicLink -Force -Target "$assets/51Degrees.csv" -Path "$RepoName/fiftyone_devicedetection_cloud/tests/51Degrees.csv"

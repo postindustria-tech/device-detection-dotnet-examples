05137c2972a1dfc6d71e6a9398ab1098f8291391
Maksym Kucherov <maximus.the0@gmail.com>
2024-10-16 12:56:19 -0400

    Enable `Cloud/GettingStarted-Web` to display suppressed errors.

--- Examples/Cloud/GettingStarted-Web/Model/IndexModel.cs
+++ Examples/Cloud/GettingStarted-Web/Model/IndexModel.cs
@@ -30,26 +30,28 @@ namespace FiftyOne.DeviceDetection.Examples.Cloud.GettingStartedWeb.Model
 {
     public class IndexModel
     {
-        public string HardwareVendor { get; private set; }
-        public string HardwareName { get; private set; }
-        public string DeviceType { get; private set; }
-        public string PlatformVendor { get; private set; }
-        public string PlatformName { get; private set; }
-        public string PlatformVersion { get; private set; }
-        public string BrowserVendor { get; private set; }
-        public string BrowserName { get; private set; }
-        public string BrowserVersion { get; private set; }
-        public string ScreenWidth { get; private set; }
-        public string ScreenHeight { get; private set; }
-        public string DeviceId { get; private set; }
+        public string HardwareVendor { get; private init; }
+        public string HardwareName { get; private init; }
+        public string DeviceType { get; private init; }
+        public string PlatformVendor { get; private init; }
+        public string PlatformName { get; private init; }
+        public string PlatformVersion { get; private init; }
+        public string BrowserVendor { get; private init; }
+        public string BrowserName { get; private init; }
+        public string BrowserVersion { get; private init; }
+        public string ScreenWidth { get; private init; }
+        public string ScreenHeight { get; private init; }
+        public string DeviceId { get; private init; }
 
-        public IFlowData FlowData { get; private set; }
+        public IFlowData FlowData { get; private init; }
 
-        public CloudRequestEngine Engine { get; private set; }
+        public CloudRequestEngine Engine { get; private init; }
 
-        public List<EvidenceModel> Evidence { get; private set; }
+        public IEnumerable<EvidenceModel> Evidence { get; private init; }
 
-        public IHeaderDictionary ResponseHeaders { get; private set; }
+        public IEnumerable<ErrorModel> Errors { get; private init; }
+
+        public IHeaderDictionary ResponseHeaders { get; private init; }
 
         public IndexModel(IFlowData flowData, IHeaderDictionary responseHeaders)
         {
@@ -58,15 +60,30 @@ namespace FiftyOne.DeviceDetection.Examples.Cloud.GettingStartedWeb.Model
 
             // Get the cloud engine
             Engine = FlowData.Pipeline.GetElement<CloudRequestEngine>();
+
+            // Try get key filter
+            IEvidenceKeyFilter evidenceKeyFilter;
+            try
+            {
+                evidenceKeyFilter = Engine.EvidenceKeyFilter;
+            }
+            catch 
+            {
+                evidenceKeyFilter = null;
+            }
+
             // Get the evidence that was used when performing device detection.
             Evidence = FlowData.GetEvidence().AsDictionary()
                 .Select(e => new EvidenceModel()
                 {
                     Key = e.Key,
                     Value = e.Value.ToString(),
-                    Used = Engine.EvidenceKeyFilter.Include(e.Key)
+                    Used = evidenceKeyFilter?.Include(e.Key),
                 })
                 .ToList();
+
+            // Get errors
+            Errors = FlowData.Errors?.Select(flowError => new ErrorModel(flowError)).ToList();
 
             // Get the results of device detection.
             var deviceData = FlowData.Get<IDeviceData>();

92a9da5675cef1e1fbff6590d8647eb471e7bd52
Maksym Kucherov <maximus.the0@gmail.com>
2025-04-09 12:34:51 +0300

    CI: Move csproj list from ps1 to SLNF.

--- ci/build-project.ps1
+++ ci/build-project.ps1
@@ -7,40 +7,19 @@ param(
     [string]$Arch = "x64",
     [string]$BuildMethod = "msbuild"
 )
+$ErrorActionPreference = "Stop"
+$PSNativeCommandUseErrorActionPreference = $true
 
 if ($BuildMethod -eq "dotnet"){
-    Write-Output "$Configuration"
-    $Projects = @( 
-    ".\Examples\ExampleBase\FiftyOne.DeviceDetection.Examples.csproj",
-    ".\Examples\Legacy Web\Cloud-UACH\Cloud - Client-Hints.csproj",
-    ".\Examples\Legacy Web\Cloud-UACH-manual\Cloud - Client-Hints Not Integrated.csproj",
-    ".\Examples\Legacy Web\UACH\Client-Hints.csproj",
-    ".\Examples\Legacy Web\UACH-manual\Client-Hints Not Integrated.csproj",
-    ".\Examples\Cloud\Configurator-Console\Configurator-Console.csproj",
-    ".\Examples\Cloud\GetAllProperties-Console\GetAllProperties.csproj",
-    ".\Examples\Cloud\GettingStarted-Console\GettingStarted-Console.csproj",
-    ".\Examples\Cloud\GettingStarted-Web\GettingStarted-Web.csproj",
-    ".\Examples\Cloud\Metadata-Console\Metadata-Console.csproj",
-    ".\Examples\Cloud\NativeModel-Console\NativeModelLookup-Console.csproj",
-    ".\Examples\Cloud\TAC-Console\TacLookup-Console.csproj",
-    ".\Examples\OnPremise\GettingStarted-Console\GettingStarted-Console.csproj",
-    ".\Examples\OnPremise\GettingStarted-Web\GettingStarted-Web.csproj",
-    ".\Examples\OnPremise\MatchMetrics-Console\MatchMetrics-Console.csproj",
-    ".\Examples\OnPremise\Metadata-Console\Metadata-Console.csproj",
-    ".\Examples\OnPremise\OfflineProcessing-Console\OfflineProcessing-Console.csproj",
-    ".\Examples\OnPremise\Performance-Console\Performance-Console.csproj",
-    ".\Examples\OnPremise\UpdateDataFile-Console\UpdateDataFile-Console.csproj",
-    ".\Tests\FiftyOne.DeviceDetection.Example.Tests.Web\FiftyOne.DeviceDetection.Example.Tests.Web.csproj"
-    )
-
-    foreach($Project in $Projects){
-        ./dotnet/build-project-core.ps1 -RepoName $RepoName -ProjectDir $Project -Name $Name -Configuration $Configuration -Arch $Arch
-    }
-
-
+    # Use the solution filter to remove the Framework projects.
+    ./dotnet/build-project-core.ps1 `
+        -RepoName $RepoName `
+        -ProjectDir "$ProjectDir/FiftyOne.DeviceDetection.Examples.Core.slnf" `
+        -Name $Name `
+        -Configuration $Configuration `
+        -Arch $Arch
 }
 else{
-
     ./dotnet/build-project-framework.ps1 -RepoName $RepoName -ProjectDir $ProjectDir -Name $Name -Configuration $Configuration -Arch $Arch
 }
 
